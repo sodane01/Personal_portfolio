2d5efd41fe9ff950701f38652a48c7dfc8fa425a
Anette Lindgren Söderström <sodane01@gmail.com>
2026-06-15 17:21:25 +0200

    New base code

---
+++ portfolio-website/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <h2>Anette Söderström</h2>
+      <p>Software Tester & QA Specialist</p>
+    </main>
+  );
+}
+
+export default Home;
